9038f45c226f6db521167ccc2be25fd5f4e6c277
Alice Smith <alice@webage.com>
2025-03-20 10:05:44 -0400

    css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 const NavBar = () => {
   return (
-    <nav>
-      <div className="container">
-        <Link to="/" className="navbar-brand">
-          Home
-        </Link>
-      </div>
-    </nav>
+    <div>
+      <ul>
+        <li>
+          <Link to="/" className="navbar-brand">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className="navbar-brand">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="navbar-brand">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
