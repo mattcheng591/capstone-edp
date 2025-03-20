7503655afbd11640f550b9ec1c4596b7911a9c34
Alice Smith <alice@webage.com>
2025-03-20 13:27:48 -0400

    css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -33,25 +33,37 @@ const Home = ({ shoes, cart, addToCart, removeFromCart }) => {
       <div className="row mt-4page">
         {shoes.slice(0, 4).map((shoe) => (
           <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={shoe.shoeId}>
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="card-title">
+            <div
+              className="ag-courses_item"
+              onClick={() => addToCart(shoe)} // Add onClick to the entire card
+              style={{ cursor: "pointer" }} // Optional: Add pointer cursor for better UX
+            >
+              <div className="ag-courses-item_link">
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">
                   {shoe.shoe_brand} - {shoe.shoe_type}
-                </h5>
-                <img
-                  src="https://static.nike.com/a/images/w_1280,q_auto,f_auto/354d14df-3286-44e6-afbf-debaf283d320/air-jordan-1-low-x-travis-scott-sail-and-ridgerock-dm7866-162-release-date.jpg" // Assuming 'image' contains the URL of the shoe image
-                  alt={`${shoe.shoe_brand} - ${shoe.shoe_type}`}
-                  className="img-fluid mb-3" // Bootstrap class for responsive images
-                />
-                <p className="card-text">Color: {shoe.shoe_color}</p>
-                <p className="card-text">Size: {shoe.shoe_size}</p>
-                <p className="card-text">Price: ${shoe.price}</p>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => addToCart(shoe)}
-                >
-                  Add to Cart
-                </button>
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                  Color:{" "}
+                  <span className="ag-courses-item_date">
+                    {shoe.shoe_color}
+                  </span>
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                  Size:{" "}
+                  <span className="ag-courses-item_date">{shoe.shoe_size}</span>
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                  Price:{" "}
+                  <span className="ag-courses-item_date">${shoe.price}</span>
+                </div>
+                <div className="ag-courses-item_date-box">
+                  Click to add to cart
+                </div>
               </div>
             </div>
           </div>
@@ -63,26 +75,35 @@ const Home = ({ shoes, cart, addToCart, removeFromCart }) => {
       <Filter onFilter={handleFilter} setShoes={setSearchedShoes} />
       <div className="row mt-4">
         {searchedShoes.map((shoe) => (
-          <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={shoe.shoeId}>
-            <div className="card h-100">
-              <div className="card-body">
-                <h5 className="card-title">
-                  {shoe.shoe_brand} - {shoe.shoe_type}
-                </h5>
-                <img
-                  src="https://static.nike.com/a/images/w_1280,q_auto,f_auto/354d14df-3286-44e6-afbf-debaf283d320/air-jordan-1-low-x-travis-scott-sail-and-ridgerock-dm7866-162-release-date.jpg" // Assuming 'image' contains the URL of the shoe image
-                  alt={`${shoe.shoe_brand} - ${shoe.shoe_type}`}
-                  className="img-fluid mb-3" // Bootstrap class for responsive images
-                />
-                <p className="card-text">Color: {shoe.shoe_color}</p>
-                <p className="card-text">Size: {shoe.shoe_size}</p>
-                <p className="card-text">Price: ${shoe.price}</p>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => addToCart(shoe)}
-                >
-                  Add to Cart
-                </button>
+          <div
+            className="ag-courses_item"
+            key={shoe.shoeId}
+            onClick={() => addToCart(shoe)}
+            style={{ cursor: "pointer" }}
+          >
+            <div className="ag-courses-item_link">
+              <div className="ag-courses-item_bg"></div>
+
+              <div className="ag-courses-item_title">
+                {shoe.shoe_brand} - {shoe.shoe_type}
+              </div>
+
+              <div className="ag-courses-item_date-box">
+                Color:{" "}
+                <span className="ag-courses-item_date">{shoe.shoe_color}</span>
+              </div>
+
+              <div className="ag-courses-item_date-box">
+                Size:{" "}
+                <span className="ag-courses-item_date">{shoe.shoe_size}</span>
+              </div>
+
+              <div className="ag-courses-item_date-box">
+                Price:{" "}
+                <span className="ag-courses-item_date">${shoe.price}</span>
+              </div>
+              <div className="ag-courses-item_date-box">
+                Click to add to cart
               </div>
             </div>
           </div>
